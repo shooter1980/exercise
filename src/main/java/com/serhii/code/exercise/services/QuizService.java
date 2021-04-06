ffe00208fd4e2d8c6d1b3e221e38c92a385e0ee2
shooter1980 <shooter.adm@gmail.com>
2021-04-06 10:16:09 -0400

    refactoring error check moving url in properties

--- src/main/java/com/serhii/code/exercise/services/QuizService.java
+++ src/main/java/com/serhii/code/exercise/services/QuizService.java
@@ -1,6 +1,8 @@
 package com.serhii.code.exercise.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,13 +11,20 @@ import reactor.core.publisher.Mono;
 @Scope("prototype")
 public class QuizService implements QuizServiceI{
 
+    @Value( "${exercise.url}" )
+    private String url;
+
     @Override
     public Mono<ServiceResult> getResult(Integer amount, Integer category) {
-        WebClient webClient = WebClient.create("https://opentdb.com/api.php");
+        WebClient webClient = WebClient.create(url);
         return webClient
                 .get()
                 .uri(String.join("", "?","amount=", amount.toString(), "&","category=", category.toString()))
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError,
+                        error -> Mono.error(new RuntimeException("Remote API not found")))
+                .onStatus(HttpStatus::is5xxServerError,
+                        error -> Mono.error(new RuntimeException("Remote server is not responding")))
                 .bodyToMono(ServiceResult.class);
     }
 
