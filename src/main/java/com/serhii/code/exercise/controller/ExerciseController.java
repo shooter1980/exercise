b6dff590c7255ea819938b3c8652a5eca60a57fd
shooter1980 <shooter.adm@gmail.com>
2021-05-09 12:24:25 -0400

    refactoring getQuiz()

--- src/main/java/com/serhii/code/exercise/controller/ExerciseController.java
+++ src/main/java/com/serhii/code/exercise/controller/ExerciseController.java
@@ -5,12 +5,12 @@ import com.serhii.code.exercise.jdbc.ExerciseResponse;
 import com.serhii.code.exercise.jdbc.ResponseRepository;
 import com.serhii.code.exercise.services.CollectServiceI;
 import com.serhii.code.exercise.services.TransformationServiceI;
+import io.reactivex.rxjava3.core.Observable;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -36,14 +36,15 @@ public class ExerciseController {
             summary = "get quiz result"
     )
     @GetMapping("/coding/exercise/quiz")
-    public Response getQuiz() throws ExecutionException, InterruptedException {
-        Response response = collectService.collect().map(transformationService::transform)
-                .toFuture().get();
+    public Mono<Response> getQuiz() throws ExecutionException, InterruptedException {
+        Mono<Response> responseMono = collectService.collect().map(transformationService::transform);
         ExerciseResponse exerciseResponse = new ExerciseResponse();
         exerciseResponse.setId(LocalTime.now().toNanoOfDay());
-        exerciseResponse.setResponse(response);
-        responseRepository.save(exerciseResponse);
-        return response;
+        responseMono.subscribe(response -> {
+                    exerciseResponse.setResponse(response);
+                    responseRepository.save(exerciseResponse);
+                });
+        return responseMono;
     }
 
 
@@ -52,12 +53,11 @@ public class ExerciseController {
     )
     @GetMapping("/coding/exercise/repo/get/{id}")
     public Response getFromRepo(@PathVariable Long id) {
-
         Optional<ExerciseResponse> byId = responseRepository.findById(id);
         if (byId.isPresent()) {
             return byId.get().getResponse();
         } else {
-            return null;
+            return new Response();
         }
     }
 
